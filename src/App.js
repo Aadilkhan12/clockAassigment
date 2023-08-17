0a925f72eeb936b7f3b5d5fb8a8c47b1a47a1009
Aadil <90678881+Aadilkhan12@users.noreply.github.com>
2023-08-17 18:49:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [showDate, setShowDate] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = daysOfWeek[currentTime.getDay()];
+  const formattedDate = currentTime.toDateString();
+
+  const hours = currentTime
+    .getHours()
+    .toString()
+    .padStart(2, "0");
+  const minutes = currentTime
+    .getMinutes()
+    .toString()
+    .padStart(2, "0");
+  const seconds = currentTime
+    .getSeconds()
+    .toString()
+    .padStart(2, "0");
+
+  const toggleDisplay = () => {
+    setShowDate(!showDate);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="clock-container">
+        <div className="clock">
+          <div className="time">
+            <span>{hours}</span>
+            <span>:</span>
+            <span>{minutes}</span>
+            <span>:</span>
+            <span>{seconds}</span>
+          </div>
+          {showDate && (
+            <div className="date">
+              <p>{formattedDate}</p>
+              <p>{dayOfWeek}</p>
+            </div>
+          )}
+        </div>
+        <button className="toggle-btn" onClick={toggleDisplay}>
+          {showDate ? "Hide Date" : "Show Date"}
+        </button>
+      </div>
+      <footer className="footer">
+        <p>&copy; Aadil {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
